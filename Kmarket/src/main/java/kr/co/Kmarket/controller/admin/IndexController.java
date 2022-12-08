ffe9ea1a51f01d7628e0e5fce94012555033372c
kimwoo232800 <kimwoo232800@naver.com>
2022-12-08 17:58:44 +0900

    header, footer add

--- Kmarket/src/main/java/kr/co/Kmarket/controller/admin/IndexController.java
+++ Kmarket/src/main/java/kr/co/Kmarket/controller/admin/IndexController.java
@@ -1,6 +1,16 @@
 package kr.co.Kmarket.controller.admin;
 
+<<<<<<< HEAD
 
 public class IndexController  {
+=======
+import javax.servlet.http.HttpServlet;
+
+public class IndexController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+>>>>>>> jinwoo
 
 }
